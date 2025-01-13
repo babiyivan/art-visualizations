aec45027d55ea56e0ac372b097ae783b166a9946
sla-lev <e1634250@student.tuwien.ac.at>
2025-01-13 02:28:15 +0100

    [ADDED] Bubble Map prototype

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 import * as fs from 'fs';
 import './styles.css';
-import {initMap } from "./map";
+import {initMap, saveCoords } from "./map";
 import { initSlider } from "./slider";
 import * as data from './data.js';
+import {map} from "leaflet/src/map";
 // import bootstrap from 'bootstrap'
 
-let csvData;
+// let csvData;
+//
+// data.readData().then((d) => {
+//     csvData = d.filter(row =>
+//         Object.keys(row).every(key => row[key] !== '')
+//     );
+//
+//     console.log("minyear: " + data.minYear(csvData));
+//     console.log("maxyear: " + data.maxYear(csvData));
+//     console.log(csvData[0]["a_firstname"]);
+//
+//     d.group(csvData, d => d.a_firstname);
+//     console.log(csvData);
+// });
+// // console.log(csvData)
 
-data.readData().then((d) => {
-    csvData = d.filter(row =>
+const csvPromise = fetch('http://localhost:8081/src/data/artvis_dump_NEW.csv')
+    .then(response => response.text())
+    .then(csvText => d3.csvParse(csvText));
+
+csvPromise.then(async csvData => {
+    const filteredCsvData = csvData.filter(row =>
         Object.keys(row).every(key => row[key] !== '')
     );
 
-    console.log("minyear: " + data.minYear(csvData));
-    console.log("maxyear: " + data.maxYear(csvData));
-    console.log(csvData[0]["a_firstname"]);
+    // console.log("minyear: " + data.minYear(filteredCsvData));
+    // console.log("maxyear: " + data.maxYear(filteredCsvData));
+    // console.log(filteredCsvData[0]["a_firstname"]);
+
+    const groupedCsvData = d3.group(filteredCsvData, d => d.e_longitude, d => d.e_latitude);
+    console.log(groupedCsvData);
+    await saveCoords(groupedCsvData);
+    // drawMarkers();
 
 });
-// console.log(csvData)
 
 
 initMap();
--- src/map.js
+++ src/map.js
@@ -1,9 +1,41 @@
 import * as leaflet from 'leaflet';
+import * as d3 from 'd3';
 
 let map = L.map('map').setView([48.0, 10.0], 3);
+let coords;
+
 
 map.on('zoomend', function() {
     console.log('zoom level: ' + map.getZoom());
+    map.eachLayer(function (layer) {
+        if (layer instanceof L.Circle) {
+            map.removeLayer(layer);
+        }
+    });
+    for (const [key, value] of coords) {
+        // console.log(`key: ${key}, value: ${value}`);
+        // console.log(value);
+        // console.log("count:");
+        // console.log(value.size);
+        for (const [key2, value2] of value) {
+            console.log("key: ");
+            console.log(key);
+            console.log("key2:");
+            console.log(key2);
+            console.log("size: ");
+            console.log(value2.length);
+            if (key !== "\\N" && key2 !== "\\N") {
+                let radCoefficient = 256 * Math.pow(2, map.getZoom());
+                var circle = L.circle([key2, key], {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    radius: 5000000*Math.sqrt(value2.length) / (radCoefficient*0.5)
+
+                }).addTo(map);
+            }
+        }
+    }
 });
 map.on('moveend', function() {
     console.log('center: ' + map.getCenter());
@@ -15,7 +47,70 @@ export function initMap() {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+}
 
 
+// export function drawMarkers() {
+//     if(typeof this.coords === 'undefined') {
+//         setTimeout(drawMarkers, 100);
+//         return;
+//     }
+//     // var marker = L.marker([long, lat]).addTo(map);
+//     // var circle = L.circle([long, lat], {
+//     //     color: 'red',
+//     //     fillColor: '#f03',
+//     //     fillOpacity: 0.5,
+//     //     radius: rad*100/map.getZoom()
+//     // }).addTo(map);
+//     for (const [key, value] of coords) {
+//         // console.log(`key: ${key}, value: ${value}`);
+//         // console.log(value);
+//         // console.log("count:");
+//         // console.log(value.size);
+//         for (const [key2, value2] of value) {
+//             console.log("key: ");
+//             console.log(key);
+//             console.log("key2:");
+//             console.log(key2);
+//             console.log("size: ");
+//             console.log(value2.length);
+//             if (key !== "\\N" && key2 !== "\\N") {
+//                 var circle = L.circle([long, lat], {
+//                     color: 'red',
+//                     fillColor: '#f03',
+//                     fillOpacity: 0.5,
+//                     radius: rad*100/map.getZoom()
+//                 }).addTo(map);
+//             }
+//         }
+//     }
+// }
 
+
+export async function saveCoords(input) {
+    coords = input;
+
+    for (const [key, value] of coords) {
+        // console.log(`key: ${key}, value: ${value}`);
+        // console.log(value);
+        // console.log("count:");
+        // console.log(value.size);
+        for (const [key2, value2] of value) {
+            console.log("key: ");
+            console.log(key);
+            console.log("key2:");
+            console.log(key2);
+            console.log("size: ");
+            console.log(value2.length);
+            if (key !== "\\N" && key2 !== "\\N") {
+                let radCoefficient = 256 * Math.pow(2, map.getZoom());
+                var circle = L.circle([key2, key], {
+                    color: 'red',
+                    fillColor: '#f03',
+                    fillOpacity: 0.5,
+                    radius: 5000000*Math.sqrt(value2.length) / (radCoefficient*0.5)
+                }).addTo(map);
+            }
+        }
+    }
 }
